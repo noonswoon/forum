7a24559f79595921c288d9d78f612e0f7252b75d
theeranitp <theeranitp@gmail.com>
2014-11-12 17:49:40 +0700

    remove c# code from config project

--- Noonswoon.Community.Configuration/XmlConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Noonswoon.Community.Configuration
-{
-    public class XmlConfig 
-    {
-        public virtual string SourceName { get; set; }
-        public virtual string Setting { get; set; }
-    }
-}
