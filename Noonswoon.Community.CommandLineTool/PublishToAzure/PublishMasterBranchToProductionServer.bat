09a702fcdef0b9286d777d18b091abd1295709aa
theeranitp <theeranitp@gmail.com>
2014-11-29 23:12:44 +0700

    update configuration for production deployment to golfdig

--- Noonswoon.Community.CommandLineTool/PublishToAzure/PublishMasterBranchToProductionServer.bat
+++ Noonswoon.Community.CommandLineTool/PublishToAzure/PublishMasterBranchToProductionServer.bat
@@ -3,8 +3,12 @@ echo off
 nant /f:PublishToAzure.build ^
 -D:branch=master ^
 -D:visualstudio.config=Release ^
--D:service=nscompro ^
--D:storage=nscomprostorage ^
--D:environment=production
+-D:service=gdnscommunitypro ^
+-D:storage=gdnscommunityprostorage ^
+-D:environment=production ^
+-D:solutionName="MVCForum" ^
+-D:cloudProjectName="Noonswoon.Community.Azure" ^
+-D:publishSettingName="golfdigg" ^
+-D:subscriptionName="BizSpark Plus"
 
 pause
